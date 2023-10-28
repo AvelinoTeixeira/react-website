640399262769051989099ac966a58e44eb15c3ea
AvelinoTeixeira <129909440+AvelinoTeixeira@users.noreply.github.com>
2023-10-28 13:30:22 +0100

    Initial commit

---
+++ src/data/HeroData.js
@@ -0,0 +1,47 @@
+export const heroOne = {
+	reverse: true,
+	inverse: true,
+	topLine: {
+		text: 'Fundada em 2018',
+	},
+	headline: "Estamos no mercado há 5 anos",
+	description: 'Projetamos e implementamos as melhores soluções de segurança em nuvem',
+	buttonLabel: 'Encontre Mais',
+	imgStart: 'start',
+	img: './assets/svg/Deal.svg',
+	start: 'true',
+};
+
+export const heroTwo = {
+	
+	reverse: false,
+	inverse: false,
+	topLine: {
+		text: 'Projetado e desenvolvido',
+	},
+	headline: 'As melhores práticas',
+	description: 'Nossos clientes tiveram a melhor experiência trabalhando conosco',
+	buttonLabel: 'Ver Projecto',
+
+	linkTo: '/more',
+	imgStart: 'start',
+	img: './assets/svg/Connection.svg',
+	start: 'true',
+};
+
+export const heroThree = {
+	reverse: true,
+	inverse: true,
+	topLine: {
+		text: 'Marca de grande reputação',
+	},
+	headline: 'Por que nós? ',
+	description:
+		'A nossa experiência de 5 anos permitiu-nos utilizar as tecnologias e metodologias mais inovadoras',
+	buttonLabel: 'Ver Projecto',
+
+	linkTo: '/download',
+	imgStart: '',
+	img: './assets/svg/ChartUp.svg',
+	start: 'true',
+};
---
+++ src/data/PricingData.js
@@ -0,0 +1,40 @@
+export const pricingData = [
+	{
+		title: 'Básico',
+		price: '$7.99 usuário/mês após o período da oferta',
+		numAcc: '50-100 accounts',
+		features: ['Contas seguras', 'Melhor para pequenas empresas', 'Fácil', 'Acessível', '1GB RAM'],
+	},
+	{
+		title: 'Padrão',
+		price: '$18.99 usuário/mês após o período da oferta',
+		numAcc: '50-500 accounts',
+		features: [
+			'Contas seguras',
+			'Melhor para pequenas empresas',
+			'Customizável',
+			'3GB RAM',
+			'1TB SSD',
+		],
+	},
+	{
+		title: 'Prêmio',
+		price: '$32.50 usuário/mês após o período da oferta',
+		numAcc: '50-1000 accounts',
+		features: [
+			'Contas seguras',
+			'Melhor para pequenas empresas',
+			'Customizável',
+			'Fácil Integração',
+			'Equipe de desenvolvimento',
+			'DNS e domínio incluídos',
+		],
+	},
+
+	{
+		title: 'Empreendimento',
+		price: '$55.50 usuário/mês após o período da oferta ',
+		numAcc: '1000+ Accounts',
+		features: ['Armazenamento ilimitado', 'Escalabilidade', 'DNS e domínio grátis'],
+	},
+];
